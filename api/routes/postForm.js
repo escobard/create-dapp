07794ac358ff48be9d9f54c255335a140cfc1f11
Daniel Escobar <escobard90@gmail.com>
2019-05-09 16:49:54 -0600

    api-postForm - feat: added base route logic

---
+++ api/routes/postForm.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// the route here is replaced by the route passed within ./index.js
+router.post('/', (req, res) => {
+  console.log('/postForm POST request: ', req.headers)
+
+  // TODO - update this route, determine server status (running on local vs prod) to navigator, version number of the app, etc
+  res.status(200).json(
+    {
+      healthy: true,
+      process: global.environment,
+    });
+});
+
+module.exports = router;
