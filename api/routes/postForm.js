32a7c7c7d6b3357351439aa48c2374c096fe42a2
Daniel Escobar <escobard90@gmail.com>
2019-05-09 17:09:39 -0600

    api-postForm - feat: tested form, working!

--- api/routes/postForm.js
+++ api/routes/postForm.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // the route here is replaced by the route passed within ./index.js
 router.post('/', (req, res) => {
-  console.log('/postForm POST request: ', req.headers)
+  console.log('/postForm POST request: ', req.headers);
 
   // TODO - update this route, determine server status (running on local vs prod) to navigator, version number of the app, etc
   res.status(200).json(
