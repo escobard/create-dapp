02103e96a11e23853bcd162e5a5b90686be98670
Daniel Escobar <escobard90@gmail.com>
2019-05-09 18:07:06 -0600

    api-postForm - test: added postForm base test

--- api/tests/routes/404.test.js
+++ api/tests/routes/404.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 
 // TODO - base tests for API to be updated for all Share routes
-describe("Testing all routes", () => {
+describe("Testing 404 route", () => {
   let server;
 
   beforeEach((done) => {
@@ -13,7 +13,7 @@ describe("Testing all routes", () => {
     server.close();
     done();
   });
-  
+
   it("404 everything else", done => {
     request(server)
       .get("/foo/bar")
--- api/tests/routes/postForm.test.js
+++ api/tests/routes/postForm.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 
-describe("Testing health route", () => {
+describe("Testing postForm route", () => {
   let server;
 
   // TODO find a way to make this re-usable
@@ -14,9 +14,18 @@ describe("Testing health route", () => {
     done();
   });
 
-  it("responds to /health", done => {
+  it("route passes data if when valid", done => {
+
+    const validData = {
+      "stringType":"",
+      "stringLength":"1234556789",
+      "numberType":1,
+      "numberMax":10
+    }
+
     request(server)
-      .get("/health")
+      .post("/postForm")
+      .send(validData)
       .expect(
         {
           healthy: true,
