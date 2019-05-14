93a143aefec2c3ce216b22d7c5e12d1ea53c1058
Daniel Escobar <escobard90@gmail.com>
2019-05-13 21:33:18 -0600

    ui-refactor-epic - feat: added postForm constant fields

--- ui/src/constants/index.js
+++ ui/src/constants/index.js
@@ -40,6 +40,39 @@ export const fetchDonationFields = [
   }
 ];
 
+
+export const postFormFields = [
+  {
+    name: "stringType",
+    label: "String Type",
+    placeholder: "Enter a random string",
+    value: "",
+    error: false
+  },
+  {
+    name: "stringLength",
+    label: "String Length",
+    placeholder: "Enter a random string, must have char length greater than 10",
+    value: "",
+    error: false
+  },
+  {
+    name: "numberType",
+    label: "Number Type",
+    placeholder: "Enter a random number, must have char length greater than 10",
+    value: "",
+    error: false
+  },
+  {
+    name: "numberMax",
+    label: "Number Max",
+    placeholder: "Enter a random number, must be greater than 10",
+    value: "",
+    error: false
+  },
+];
+
+
 const environment =
   process.env.NODE_ENV === "production" ? "heroku" : "development";
 
