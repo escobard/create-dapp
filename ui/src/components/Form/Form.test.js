4557864533c984fa2f62eeee9a5e1ac7795592c4
Daniel Escobar <escobard90@gmail.com>
2019-05-16 22:07:13 -0600

    ui-docker-dev - feat: ejected create-react-app, ran watch test with jest

--- ui/src/components/Form/Form.test.js
+++ ui/src/components/Form/Form.test.js
@@ -5,7 +5,7 @@ import { postFormFields } from "../../constants";
 
 describe("Form snapshot renders", () => {
   it("should render makeDonation form and fields correctly", () => {
-    const component = shallow(<DynamicForm fields={postFormFields} />);
+    const component = shallow(<DynamicForm fields={postormFields} />);
 
     expect(component).toMatchSnapshot();
   });
