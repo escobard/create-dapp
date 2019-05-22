e80809d8cbb21d57ae21f6e8e7e0a94357b4304e
Daniel Escobar <escobard90@gmail.com>
2019-05-21 23:34:19 -0600

    feat: added Dockerfile.dev && Dockerfile.test

--- ui/src/App.js
+++ ui/src/App.js
@@ -164,7 +164,7 @@ class App extends Component {
     return (
       <main className="application">
         <Navigation />
-        <h1>HI ATB</h1>
+        <h1>HI sATB</h1>
         <section className="float">
           <Form
             postForm={this.postForm}
--- ui/src/components/Footer/Footer.test.js
+++ ui/src/components/Footer/Footer.test.js
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 import Footer from "./index";
 
 describe("Footer snapshot renders", () => {
-  it("should render correctly mode", () => {
+  it("should render correctsssly mode", () => {
     const component = shallow(<Footer />);
 
     expect(component).toMatchSnapshot();
