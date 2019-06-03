06864a5d6aa912403d3a938b0201473a4e6fc72c
Daniel Escobar <escobard90@gmail.com>
2019-06-03 09:09:40 -0600

    ethereum-base-setup - refact: updated deployment logic

--- ethereum/migrations/3_deploy_charity_role.js
+++
@@ -1,8 +0,0 @@
-const CharityRole = artifacts.require("./CharityRole.sol"),
-  OwnerRole = artifacts.require("./OwnerRole.sol");
-
-module.exports = function(deployer) {
-  // deploys initial OwnerRole contracts
-  deployer.deploy(CharityRole, OwnerRole.address, {gas: 3000000})
-
-};
