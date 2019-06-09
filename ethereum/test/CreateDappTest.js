fec332c48a30141450ad272de1c547abc8c6c9b0
Daniel Escobar <escobard90@gmail.com>
2019-06-09 00:45:08 -0600

    ethereum-base-testing - feat: tdd fetch payment base test

--- ethereum/test/CreateDappTest.js
+++ ethereum/test/CreateDappTest.js
@@ -33,55 +33,23 @@ contract("CreateDapp", accounts => {
       assert.equal(paymentID, 2);
     });
   });
-});
-  /*
-  describe("Tests makeDonation()", () => {
-    it("donor can create donation, checks donation iteration", async () => {
-      await this.contract.makeDonation({ from: donor, value: amount });
-      let response = await this.contract.fetchDonationID();
 
-      assert.equal(response, 2);
-    });
-
-    /*
-        // only charity needs to be tested, can add cases for
-        it("charity cannot create donation", async () =>{
-            let response = await this.contract.makeDonation({from: charity, value: amount});
-
-            assert.equal(response, undefined);
-        })
-
-  });
-
-  describe("Tests fetchDonation()", () => {
-    // initializes contract every time prior to makeDonation();
-
-    let charityAmount = amount * 0.95,
-      lotteryAmount = amount * 0.04,
-      ownerAmount = amount * 0.01;
+  describe("Tests fetchPayments", () => {
 
     beforeEach(async () => {
-      await this.contract.initiateContract(lottery, charity, { from: owner });
+      await this.contract.makePayment({ from: owner, value: amount });
     });
 
-    it("donor can fetch donation by donationID", async () => {
-      await this.contract.makeDonation({ from: donor, value: amount });
-      let fetchedDonation = await this.contract.fetchDonation(1, {
-        from: owner
-      });
-      console.log(fetchedDonation);
+    it("owner can fetch payments by id", async () => {
 
-      // need to assert each output, due to how number amounts are returned from solidity
-      assert.equal(fetchedDonation[0], owner);
-      assert.equal(fetchedDonation[1], lottery);
-      assert.equal(fetchedDonation[2], charity);
-      assert.equal(fetchedDonation[3], donor);
-      assert.equal(fetchedDonation[4], amount);
-      assert.equal(fetchedDonation[5], charityAmount);
-      assert.equal(fetchedDonation[6], lotteryAmount);
-      assert.equal(fetchedDonation[7], ownerAmount);
-      assert.equal(fetchedDonation[8], 1);
+      let payments = await this.contract.fetchPayments(1, { from: owner });
+
+      assert.equal(paymentID, 2);
     });
+
+  });
+
+  describe("Tests emptyBalance", () => {
+
   });
 });
-        */
