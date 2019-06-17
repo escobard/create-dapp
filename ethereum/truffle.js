fcfb72582385723e065ba91c839e159e465a3201
Daniel Escobar <escobard90@gmail.com>
2019-06-16 20:38:06 -0600

    circleci-docker-compose - fix: ganache localhost

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -6,7 +6,7 @@ module.exports = {
   networks:{
     development:{
       // TODO - make this variable so that host is normal if local vs local docker
-      host: 'localhost', //"127.0.0.1", 
+      host: '0.0.0.0', //"127.0.0.1", 
       port: 8545,
       network_id: 1234,
       gas: 4700000,
