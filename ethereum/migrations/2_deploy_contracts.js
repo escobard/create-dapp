0f07113632589aff8baa49ca3fb8cdb25bac6c2e
Daniel Escobar <escobard90@gmail.com>
2019-06-17 01:16:09 -0600

    docker-ethereum-ui - feat: updated env variables for UI layer to utilize docker

--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -7,6 +7,7 @@ module.exports = function(deployer) {
     let config = {
       ganache: {
         url: "http://localhost:8545",
+        ownerAddress: Migrations.owner,
         appAddress: CreateDapp.address
       }
     };
