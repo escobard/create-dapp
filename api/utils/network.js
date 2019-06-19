42e3c3c30283c8a4fc75923298b149fd095ee620
Daniel Escobar <escobard90@gmail.com>
2019-06-18 21:40:38 -0600

    docker-ethereum-api - feat: added setOrigin helper

---
+++ api/utils/network.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+
+const setOrigin = (app) =>{
+  // TODO this needs to be refactored to a helper - needs to handle GCP logic after containerization
+  if (process.env.NODE_ENV === "production"){
+    global.environment = "prod";
+    app.use(cors({ origin: "https://share-ui.herokuapp.com" }));
+  }
+  else{
+    app.use(cors({ origin: "*" }));
+    global.environment = "dev";
+  }
+};
+
+module.exports = { setOrigin };
