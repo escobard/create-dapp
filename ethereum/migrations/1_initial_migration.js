38105e49cbba0b37f562c565834711e66fc70e5a
Daniel Escobar <escobard90@gmail.com>
2019-06-18 22:35:07 -0600

    docker-ethereum-api - debug: delete deploy_contract.js

--- ethereum/migrations/1_initial_migration.js
+++ ethereum/migrations/1_initial_migration.js
@@ -1,5 +1,21 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations).then(() =>{
+    deployer.deploy(CreateDapp).then(() => {
+      let config = {
+        ethereum: {
+          url: "http://localhost:8545",
+          ownerAddress: Migrations.owner,
+          contractAddress: CreateDapp.address
+        }
+      };
+      console.log('CONFIG', config)
+      fs.writeFileSync(
+        "./config/config.json",
+        JSON.stringify(config, null, "\t"),
+        "utf-8"
+      );
+    });
+  });
 };
--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -3,19 +3,5 @@ const CreateDapp = artifacts.require("./CreateDapp.sol"),
   fs = require("fs");
 
 module.exports = function(deployer) {
-  deployer.deploy(CreateDapp).then(() => {
-    let config = {
-      ethereum: {
-        url: "http://localhost:8545",
-        ownerAddress: Migrations.owner,
-        contractAddress: CreateDapp.address
-      }
-    };
-    console.log('CONFIG', config)
-    fs.writeFileSync(
-      "./config/config.json",
-      JSON.stringify(config, null, "\t"),
-      "utf-8"
-    );
-  });
+
 };
