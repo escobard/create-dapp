dc1ed13c0eae25d4d5fdc5ffa78513c057867039
Daniel Escobar <escobard90@gmail.com>
2019-06-22 21:48:44 -0600

    ui-share-refactor - test: updated snapshots

--- ui/src/components/Form/Form.test.js
+++ ui/src/components/Form/Form.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { shallow } from "enzyme";
 import DynamicForm from "./index";
-import { postFormFields } from "../../constants";
+import { makePaymentFields } from "../../constants";
 
 describe("Form snapshot renders", () => {
   it("should render makeDonation form and fields correctly", () => {
-    const component = shallow(<DynamicForm fields={postFormFields} />);
+    const component = shallow(<DynamicForm fields={makePaymentFields} />);
 
     expect(component).toMatchSnapshot();
   });
