3f069d15e8aef36a7c44650de3589bccfd7e8c0a
Daniel Escobar <escobard90@gmail.com>
2019-06-23 18:38:58 -0600

    fix: fixing master contract

--- api/tests/routes/health.test.js
+++ api/tests/routes/health.test.js
@@ -20,8 +20,7 @@ describe("Testing health route", () => {
       .get("/health")
       .expect({
         healthy: true,
-        ethereum: "https://rinkeby.infura.io/v3/a07ed258a1924109a285a22a3778d0bb",
-        contract: "0xC2455714dDC58d9899bdDA1eB1eB58bE5ae174bD"
+        ethereum: "https://rinkeby.infura.io/v3/a07ed258a1924109a285a22a3778d0bb"
       })
       .end((err, res) => {
         if (err) return done(err);
